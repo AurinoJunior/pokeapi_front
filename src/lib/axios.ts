69cb0a0666863e78110c3acd1f467b58c2e77fe5
Aurino Geraldo <aurinoaj@gmail.com>
2023-08-30 03:19:55 -0300

    Add .env

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:3333/pokemon",
+  baseURL: import.meta.env.VITE_BASE_API,
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
